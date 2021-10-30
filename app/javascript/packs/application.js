0f1bd336c2de6d7d04fef1bc76dd3aea518d0d66
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-10-30 20:59:13 -0300

    Add toastr-gem to flash-messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,31 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+global.toastr = require("toastr")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
