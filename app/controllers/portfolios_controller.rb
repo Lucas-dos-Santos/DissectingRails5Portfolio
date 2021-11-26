8e350e66ae8067e9b3810b89f693af1de7c25783
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-11-26 03:22:19 -0300

    Added guest user for current_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -63,7 +63,11 @@ class PortfoliosController < ApplicationController
   end
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image,
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name])
   end
 end
