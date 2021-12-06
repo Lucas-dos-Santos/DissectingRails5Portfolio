2f1b706b97ffd237c2858e7cc621ae4f30e697e0
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-12-06 04:34:40 -0300

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,38 @@ module ApplicationHelper
   def copyright_generator
     LucasDevcampViewTool::Renderer.copyright('Lucas dos Santos', 'Todos os direitos reservados.')
   end
+
+  def social_links(class_color)
+    (link_to 'https://github.com/Lucas-dos-Santos', target: '_blank' do
+      "<i class='fab fa-github text-#{class_color}'></i>".html_safe
+    end) +
+      (link_to 'https://www.instagram.com/lucas._luke', target: '_blank' do
+        '<i class="fab fa-instagram text-danger"></i>'.html_safe
+      end) +
+      (link_to 'https://www.facebook.com/lucas.dossantos.98892', target: '_blank' do
+        '<i class="fab fa-facebook"></i>'.html_safe
+      end)
+  end
+
+  def nav_items
+    [
+      { url: root_path, title: 'Home' },
+      { url: about_me_path, title: 'About' },
+      { url: contact_path, title: 'Contact' },
+      { url: blogs_path, title: 'Blog' },
+      { url: portfolios_path, title: 'Portfolio' }
+    ]
+  end
+
+  def nav_helper(style, tag_start = '', tag_end = '')
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "#{tag_start}<a href=#{item[:url]} class='#{style} #{active?(item[:url])}'>#{item[:title]}</a>#{tag_end}"
+    end
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    'active' if current_page?(path)
+  end
 end
