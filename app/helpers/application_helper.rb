ccbbcd6620c4e8c5f69dc31bfbcb38429d0fee70
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-12-08 21:06:38 -0300

    fix navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,10 +12,11 @@ module ApplicationHelper
 
   def login_helper(style = '')
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path, class: style) +
-        (link_to 'Login', new_user_session_path, class: style)
+      (link_to 'Register', new_user_registration_path, class: "#{style} #{active?(new_user_registration_path)}") +
+        (link_to 'Login', new_user_session_path, class: "#{style} #{active?(new_user_session_path)}")
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
+      link_to 'Logout', destroy_user_session_path, method: :delete,
+                                                   class: "#{style} #{active?(destroy_user_session_path)}"
     end
   end
 
