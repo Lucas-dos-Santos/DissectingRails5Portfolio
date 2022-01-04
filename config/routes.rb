7810f644c8f8f45c4f7dd49292dfed09298de892
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-01-03 22:55:56 -0300

    started i18n setup part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'login', sign_up: 'register' }
 
-  resources :portfolios do
-    put :sort, on: :collection
-  end
-  get 'angular-items', to: 'portfolios#angular'
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  resources :topics, only: %i[show]
-  resources :blogs do
-    member do
-      get :toggle_status
+  scope '(:locale)', locales: /en|pt-BR/ do
+    devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'login', sign_up: 'register' }
+    resources :portfolios do
+      put :sort, on: :collection
+    end
+    get 'about-me', to: 'pages#about'
+    get 'contact', to: 'pages#contact'
+    resources :topics, only: %i[show]
+    resources :blogs do
+      member do
+        get :toggle_status
+      end
     end
   end
   mount ActionCable.server, at: '/cable'
+  get '/:locale' => 'pages#home', :locale => /en|pt-BR/
   root to: 'pages#home'
 end
